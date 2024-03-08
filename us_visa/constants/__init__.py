ad37d4fe09c5316e1d2a212b2efda891b5e74fcd
Premkumar <patharepremkumar@gmail.com>
2024-03-08 11:56:44 +0530

    notebook drift added

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -22,3 +22,5 @@ DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
 
+SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
+
